8e2e1bce0f57d35cdad6a16ca90f5b0ba1cb2a77
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 14:34:25 +0700

    fix line

--- send_chat.php
+++ send_chat.php
@@ -72,7 +72,7 @@ function isFile($url){
 }
 function sendchat($userid,$noidung){
 global $JSON;
-$payload = '{"'.$JSON.'":"'.json_encode($noidung).'"}';
+$payload = '{"'.$JSON.'":"\\'.json_encode($noidung).'"}';
 if(isImage($noidung)) requestImage($userid, $payload);
 else if(isVoid($noidung)) requestVoid($userid, $payload);
 else if(isVideo($noidung)) requestVideo($userid, $payload);
