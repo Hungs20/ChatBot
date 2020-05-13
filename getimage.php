34b87d6493fbccd016192488cfc2f2d7d2a877f6
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 17:55:13 +0700

    update V2

--- getimage.php
+++ getimage.php
@@ -4,7 +4,7 @@ function isNudeImage($url)
 {
     $url = urlencode($url);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://api.sightengine.com/1.0/check.json?models=nudity&api_user=545033772&api_secret=qnKfMmMX5qvgSHq9oRdR&url=$url");
+    curl_setopt($ch, CURLOPT_URL, "https://api.sightengine.com/1.0/check.json?models=nudity&api_user=$NUDE_API_USER&api_secret=$NUDE_API_SECRET&url=$url");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
     
@@ -16,6 +16,7 @@ function isNudeImage($url)
     curl_close($ch);
     $result = json_decode($res, true);
     if($result['status'] == 'success'){
+       // echo 'Raw: '. $result['nudity']['raw'] . 'Safe: '. $result['nudity']['safe'] . 'Patial: '. $result['nudity']['partial'];
         if($result['nudity']['raw'] >= max($result['nudity']['safe'], $result['nudity']['partial'])) return 1;
         else if($result['nudity']['partial'] >= max($result['nudity']['raw'], $result['nudity']['safe'])) 
         {
@@ -27,7 +28,12 @@ function isNudeImage($url)
 
 $id = $_POST['id'];
 $url = $_POST['url'];
-
+if(strpos($url, 't39') === false){
+$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME); // kết nối data
+$sql = "INSERT INTO `data` (`userID`, `text`, `type`) VALUES ('$id', '$url', 'image')";
+$info = mysqli_query($conn,$sql );
+mysqli_close($conn);
+}
 $isNude = isNudeImage($url);
 if($isNude == 0)
 {
@@ -61,7 +67,7 @@ else
               "buttons":[
                 {
                   "type":"web_url",
-                  "url":"'.urlencode($url).'",
+                  "url":"'.$url.'",
                   "title":"Xem nội dung này"
                 }
               ]
