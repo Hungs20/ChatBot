c31817276f223a43928afb65fc4a976d55c992ca
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 17:43:58 +0700

    update V2

--- config.php
+++ config.php
@@ -5,6 +5,10 @@ $DBNAME = getenv('DBNAME'); // Set your database name here
 $DBPW =  getenv('DBPASS'); // Set your database password here
 $BOT_ID = getenv('BOT_ID');
 $TOKEN = getenv('BOT_TOKEN');
+
+$NUDE_API_USER = getenv('NUDE_API_USER');
+$NUDE_API_SECRET = getenv('NUDE_API_SECRET');
+
 $BLOCK_NAME = "uet_chat";
 $JSON = "uet_text";
 $BLOCK_IMAGE = "uet_image";
