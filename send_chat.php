096b07a6fc99d893e2e2881a229f0acf1244e4a7
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 13:30:03 +0700

    fix line

--- send_chat.php
+++ send_chat.php
@@ -4,6 +4,11 @@ require_once 'config.php'; //lấy thông tin từ config
 $conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME); // kết nối data
 $id = $_POST['id'];
 $noidung = $_POST['noidung'];
+
+global $JSON;
+$payload = '{"'.$JSON.'":"'.json_encode($noidung).'"}';
+echo $payload;
+
 //$noidung = substr($noidung,1,strlen($noidung) - 1);
 $errorChat = '{
      "messages": [
@@ -71,7 +76,7 @@ function isFile($url){
 }
 function sendchat($userid,$noidung){
 global $JSON;
-$payload = '{"'.$JSON.'":"'.json_encode($noidung).'"}';
+$payload = '{"'.$JSON.'":"'.$noidung.'"}';
 if(isImage($noidung)) requestImage($userid, $payload);
 else if(isVoid($noidung)) requestVoid($userid, $payload);
 else if(isVideo($noidung)) requestVideo($userid, $payload);
