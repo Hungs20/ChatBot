a74610005bce5ad77304daaf9a11fa07c6e212b8
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 12:57:32 +0700

    install database

---
+++ install.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config.php'; //lấy thông tin từ config
+$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME); // kết nối data
+
+$query = '';
+$sqlScript = file('data.sql');
+foreach ($sqlScript as $line)	{
+	
+	$startWith = substr(trim($line), 0 ,2);
+	$endWith = substr(trim($line), -1 ,1);
+	
+	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+		continue;
+	}
+		
+	$query = $query . $line;
+	if ($endWith == ';') {
+		mysqli_query($conn,$query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query. '</b></div>');
+		$query= '';		
+	}
+}
+echo '<div class="success-response sql-import-response">SQL file imported successfully</div>';
+?>
