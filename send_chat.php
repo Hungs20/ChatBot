cefd26567d441823c3fb5b280b197570e9697553
Hungs20 <tuoitre2k@gmail.com>
2020-05-13 14:29:17 +0700

    fix line

--- send_chat.php
+++ send_chat.php
@@ -88,6 +88,7 @@ function requestText($userid,$jsondata) { // hàm gửi chát :)))
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $jsondata);
+  echo htmlentities($jsondata);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
   curl_exec($ch);
 
